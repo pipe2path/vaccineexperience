8724d9a3188d18ea4877b0b83d160d6c173228c0
Kevin Menezes <kevinmenezes@yahoo.com>
2021-03-14 21:36:55 -0700

    changed api to heroku-deployed

--- src/apis/feeds.js
+++ src/apis/feeds.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:4050'
+    //baseURL: 'http://localhost:4050'
+    baseURL: ' https://covid19-menezes-api.herokuapp.com/'
 })
