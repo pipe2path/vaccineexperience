f203ffe77cf8cf7b30a9a6cfd57c16222d8ac429
Kevin Menezes <kevinmenezes@yahoo.com>
2021-03-15 17:36:51 -0700

    add img

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,12 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <div>
-            <h2 className="ui center aligned icon header">
+            <img src="vaccine-4.jpg" width="100%"/>
+            {/* <h2 className="ui center aligned icon header">
                 <i className="circular syringe icon"></i>
                     Vaccine Experience
-            </h2>
+            </h2> */}
+            <div className="ui center aligned text header">Vaccine Experience</div>
             <div className="ui divider"></div>
         </div>
     )
