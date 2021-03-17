c30edcbe287ca805467c405ac7c555ca1744ab5e
Kevin Menezes <kevinmenezes@yahoo.com>
2021-03-17 13:58:28 -0700

    fixed url

--- src/apis/feeds.js
+++ src/apis/feeds.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:4050'
-    //baseURL: ' https://covid19-menezes-api.herokuapp.com/'
+    //baseURL: 'http://localhost:4050'
+    baseURL: ' https://covid19-menezes-api.herokuapp.com/'
 })
